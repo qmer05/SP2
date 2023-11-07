161d7f32e7936d66c76b7407f6a971197aba7172
Ömer Øcalan <cph-oo203@cphbusiness.dk>
2023-11-07 18:45:21 +0100

    Initial commit

---
+++ src/main/java/ACar.java
@@ -0,0 +1,39 @@
+public abstract class ACar implements Car {
+
+    protected String registrationNumber;
+
+    protected String make;
+
+    protected String model;
+
+    protected int numberOfDoors;
+
+    public ACar(){
+        this.registrationNumber = registrationNumber;
+        this.make = make;
+        this.model = model;
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public String getRegistrationNumber(){
+        return registrationNumber;
+    }
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public int getNumberOfDoors(){
+        return numberOfDoors;
+    }
+
+    @Override
+    public String toString(){
+        return registrationNumber + make + model + numberOfDoors;
+    }
+
+}
---
+++ src/main/java/Car.java
@@ -0,0 +1,12 @@
+public interface Car {
+    public String getRegistrationNumber(); //The number on the number plate
+
+    public String getMake(); // The make of the car e.g. Audi
+
+    public String getModel(); // The model of the car e.g. A6
+
+    public int getNumberOfDoors(); // The number of doors
+
+    public int getRegistrationFee(); // Calculates the registration fee for the car
+
+}
