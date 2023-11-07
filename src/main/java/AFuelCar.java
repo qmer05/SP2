14df8906b2292e85a9a7ab37a68cffb3a0869a39
Ömer Øcalan <cph-oo203@cphbusiness.dk>
2023-11-07 20:20:43 +0100

    update

--- src/main/java/AFuelCar.java
+++ src/main/java/AFuelCar.java
@@ -2,7 +2,7 @@ public abstract class AFuelCar extends ACar {
 
     private int kmPrLitre;
 
-    public AFuelCar(String registrationNumber, String make, String model, int numberOfDoors){
+    public AFuelCar(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre){
         super(registrationNumber, make, model, numberOfDoors);
         this.kmPrLitre = kmPrLitre;
 
--- src/main/java/GasolineCar.java
+++ src/main/java/GasolineCar.java
@@ -1,16 +1,27 @@
 public class GasolineCar extends AFuelCar {
 
-    public GasolineCar(int kmPrLitre) {
-        super(kmPrLitre);
+    public GasolineCar(String registrationNumber, String make, String model, int numberOfDoors, int kmPrLitre) {
+        super(registrationNumber, make, model, numberOfDoors, kmPrLitre);
     }
 
     @Override
-    protected String getFuelType() {
+    public String getFuelType() {
         return "Gasoline";
     }
 
     @Override
     public int getRegistrationFee() {
-        return 0;
+        int kmPrLitre = getKmPrLitre();
+        if (kmPrLitre > 20 && kmPrLitre < 50) {
+            return 330;
+        } else if (kmPrLitre > 15 && kmPrLitre < 20) {
+            return 1050;
+        } else if (kmPrLitre > 10 && kmPrLitre < 15) {
+            return 2340;
+        } else if (kmPrLitre > 5 && kmPrLitre < 10) {
+            return 5500;
+        } else if (kmPrLitre < 5) {
+            return 10470;
+        } else return 0;
     }
 }
