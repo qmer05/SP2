f61a10536b97315c9b56fd56506e717ade05e93f
Ömer Øcalan <cph-oo203@cphbusiness.dk>
2023-11-08 12:32:27 +0100

    update

--- src/main/java/DieselCar.java
+++ src/main/java/DieselCar.java
@@ -15,44 +15,35 @@ public class DieselCar extends AFuelCar {
     @Override
     public int getRegistrationFee() {
 
-        if (hasParticleFilter) {
-            if (kmPrLitre >= 20 && kmPrLitre < 50) {
-                return 330 + 130;
-            } else if (kmPrLitre >= 15 && kmPrLitre < 20) {
-                return 1050 + 1390;
-            } else if (kmPrLitre >= 10 && kmPrLitre < 15) {
-                return 2340 + 1850;
-            } else if (kmPrLitre >= 5 && kmPrLitre < 10) {
-                return 5500 + 2770;
-            } else if (kmPrLitre < 5) {
-                return 10470 + 15260;
-            } else return 0;
+        int totalFee = 0;
 
-        } else if (!hasParticleFilter){
-            int particleFilterPrice = 1000;
-            if (kmPrLitre >= 20 && kmPrLitre < 50) {
-                return 330 + 130 + particleFilterPrice;
-            } else if (kmPrLitre >= 15 && kmPrLitre < 20) {
-                return 1050 + 1390 + particleFilterPrice;
-            } else if (kmPrLitre >= 10 && kmPrLitre < 15) {
-                return 2340 + 1850 + particleFilterPrice;
-            } else if (kmPrLitre >= 5 && kmPrLitre < 10) {
-                return 5500 + 2770 + particleFilterPrice;
-            } else if (kmPrLitre < 5) {
-                return 10470 + 15260 + particleFilterPrice;
-            } else return 0;
+        if (kmPrLitre >= 20 && kmPrLitre < 50) {
+            totalFee = 330 + 130;
+        } else if (kmPrLitre >= 15 && kmPrLitre < 20) {
+            totalFee = 1050 + 1390;
+        } else if (kmPrLitre >= 10 && kmPrLitre < 15) {
+            totalFee = 2340 + 1850;
+        } else if (kmPrLitre >= 5 && kmPrLitre < 10) {
+            totalFee = 5500 + 2770;
+        } else if (kmPrLitre < 5) {
+            totalFee = 10470 + 15260;
         }
-        else return 0;
+
+        if (!hasParticleFilter) {
+            return totalFee + 1000;
+        } else {
+            return totalFee;
+        }
     }
 
-    public boolean hasParticleFilter(){
+    public boolean hasParticleFilter() {
         return true;
     }
 
     @Override
-    public String toString(){
-        return "Reg. nr: "+registrationNumber+"\nMake: "+ make +"\nModel: "+model+ "\nNumber of doors: "
-                +numberOfDoors+"\nKm pr litre: "+kmPrLitre+"\nHas particle filter: "+hasParticleFilter
-                +"\nFuel Type: "+getFuelType()+"\nRegistration fee: "+getRegistrationFee() + " DKK";
+    public String toString() {
+        return "Reg. nr: " + registrationNumber + "\nMake: " + make + "\nModel: " + model + "\nNumber of doors: "
+                + numberOfDoors + "\nKm pr litre: " + kmPrLitre + "\nHas particle filter: " + hasParticleFilter
+                + "\nFuel Type: " + getFuelType() + "\nRegistration fee: " + getRegistrationFee() + " DKK";
     }
 }
